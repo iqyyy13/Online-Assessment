c10233e95f33f102a675aa22549be24d0e6284c1
Muhd Iqshan <muhdiqshan@gmail.com>
2024-03-26 22:21:56 +0800

    task 1 completed

--- Task 1.js
+++ Task 1.js
@@ -1,22 +1,10 @@
-var sum_to_n_a = function(n){
-    console.log("Received n:", n)
-
-    var x
-    var total = 0
-
-    for(x=1; x<=n; x++)
-    {
-        total += x
-    }
-
-    n = total
-
-    return n
+var sum_to_n_a = function(n)
+{
+    return (n*(n+1))/2
 };
 
-var sum_to_n_b = function(n){
-    console.log("Received n:", n)
-
+var sum_to_n_b = function(n)
+{
     var total = 0
     var i = 1
 
@@ -32,8 +20,6 @@ var sum_to_n_b = function(n){
 
 var sum_to_n_c = function(n)
 {
-    console.log("Received n:", n)
-
     if(n===1)
     {
         return 1
@@ -45,7 +31,5 @@ var sum_to_n_c = function(n)
 }
 
 console.log(sum_to_n_a(10));
-console.log("")
 console.log(sum_to_n_b(10));
-console.log("")
 console.log(sum_to_n_c(10));
