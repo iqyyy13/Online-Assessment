8313761eaa0dfa3a282372d863bd845b00d89dd3
Muhd Iqshan <muhdiqshan@gmail.com>
2024-04-03 11:38:33 +0800

    commit

--- problem2/src/App.js
+++ problem2/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import { FaArrowAltCircleDown, FaUser } from 'react-icons/fa';
+import { FaArrowAltCircleDown} from 'react-icons/fa';
 import Select from 'react-select'
 import Etherium from './components/Etherium';
 import Luna from './components/Luna';
@@ -20,46 +20,6 @@ import uniswap from "./components/uniswap.png";
 import metamask from "./components/metamask.png";
 import walletconnect from "./components/walletconnect.png";
 import coinbasewallet from "./components/coinbasewallet.png";
-
-const jsonData = 
-[
-    {"currency":"BLUR","date":"2023-08-29T07:10:40.000Z","price":0.20811525423728813},
-    {"currency":"bNEO","date":"2023-08-29T07:10:50.000Z","price":7.1282679},
-    {"currency":"BUSD","date":"2023-08-29T07:10:40.000Z","price":0.999183113},
-    {"currency":"BUSD","date":"2023-08-29T07:10:40.000Z","price":0.9998782611186441},
-    {"currency":"USD","date":"2023-08-29T07:10:30.000Z","price":1},
-    {"currency":"ETH","date":"2023-08-29T07:10:52.000Z","price":1645.9337373737374},
-    {"currency":"GMX","date":"2023-08-29T07:10:40.000Z","price":36.345114372881355},
-    {"currency":"STEVMOS","date":"2023-08-29T07:10:40.000Z","price":0.07276706779661017},
-    {"currency":"LUNA","date":"2023-08-29T07:10:40.000Z","price":0.40955638983050846},
-    {"currency":"RATOM","date":"2023-08-29T07:10:40.000Z","price":10.250918915254237},
-    {"currency":"STRD","date":"2023-08-29T07:10:40.000Z","price":0.7386553389830508},
-    {"currency":"EVMOS","date":"2023-08-29T07:10:40.000Z","price":0.06246181355932203},
-    {"currency":"IBCX","date":"2023-08-29T07:10:40.000Z","price":41.26811355932203},
-    {"currency":"IRIS","date":"2023-08-29T07:10:40.000Z","price":0.0177095593220339},
-    {"currency":"ampLUNA","date":"2023-08-29T07:10:40.000Z","price":0.49548589830508477},
-    {"currency":"KUJI","date":"2023-08-29T07:10:45.000Z","price":0.675},
-    {"currency":"STOSMO","date":"2023-08-29T07:10:45.000Z","price":0.431318},
-    {"currency":"USDC","date":"2023-08-29T07:10:40.000Z","price":0.989832},
-    {"currency":"axlUSDC","date":"2023-08-29T07:10:40.000Z","price":0.989832},
-    {"currency":"ATOM","date":"2023-08-29T07:10:50.000Z","price":7.186657333333334},
-    {"currency":"STATOM","date":"2023-08-29T07:10:45.000Z","price":8.512162050847458},
-    {"currency":"OSMO","date":"2023-08-29T07:10:50.000Z","price":0.3772974333333333},
-    {"currency":"rSWTH","date":"2023-08-29T07:10:40.000Z","price":0.00408771},
-    {"currency":"STLUNA","date":"2023-08-29T07:10:40.000Z","price":0.44232210169491526},
-    {"currency":"LSI","date":"2023-08-29T07:10:50.000Z","price":67.69661525423729},
-    {"currency":"OKB","date":"2023-08-29T07:10:40.000Z","price":42.97562059322034},
-    {"currency":"OKT","date":"2023-08-29T07:10:40.000Z","price":13.561577966101694},
-    {"currency":"SWTH","date":"2023-08-29T07:10:45.000Z","price":0.004039850455012084},
-    {"currency":"USC","date":"2023-08-29T07:10:40.000Z","price":0.994},
-    {"currency":"USDC","date":"2023-08-29T07:10:30.000Z","price":1},
-    {"currency":"USDC","date":"2023-08-29T07:10:30.000Z","price":1},
-    {"currency":"USDC","date":"2023-08-29T07:10:40.000Z","price":0.9998782611186441},
-    {"currency":"WBTC","date":"2023-08-29T07:10:52.000Z","price":26002.82202020202},
-    {"currency":"wstETH","date":"2023-08-29T07:10:40.000Z","price":1872.2579742372882},
-    {"currency":"YieldUSD","date":"2023-08-29T07:10:40.000Z","price":1.0290847966101695},
-    {"currency":"ZIL","date":"2023-08-29T07:10:50.000Z","price":0.01651813559322034}
-]
 
 const options = [
   {value: 'LUNA', label: 'Luna', icon: <Luna/>},
@@ -95,21 +55,6 @@ const CustomSingleValue = ({data}) => (
   ) : null
 );
 
-const Selector = ({id, selectedOption, setSelectedOption}) => {
-  const handleChange = (selectedOption) => {
-    setSelectedOption(selectedOption);
-  };
-
-  return (
-    <Select
-      options={options}
-      value={selectedOption}
-      onChange={handleChange}
-      placeholder={`Select option for Selector ${id}`}
-    />
-  );
-};
-
 function App() {
   const [selectedOption1, setSelectedOption1] = useState(null);
   const [selectedOption2, setSelectedOption2] = useState(null);
@@ -119,8 +64,6 @@ function App() {
   const [userInput2, setUserInput2] = useState('');
   const [amount1, setamount1] = useState('');
   const [amount2, setamount2] = useState('');
-
-  const [isSwapMode, setIsSwapMode] = useState('');
 
   const [showOverlay, setShowOverlay] = useState(false);
   const [showOverlay3, setShowOverlay3] = useState(false);
@@ -158,10 +101,6 @@ function App() {
 
     setPrice1(selectedOption2 ? cryptoPrices[selectedOption2.value] : 1);
     setPrice2(selectedOption1 ? cryptoPrices[selectedOption1.value] : 1);
-  }
-
-  const getInputLabelText = () => {
-    return isSwapMode ? 'You receive' : 'You pay';
   }
 
   const handleInputChange1 = (event) => {
